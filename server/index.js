3f2a84c0b0014849b724bad7057f5766d44b9e2b
Ari Rantalainen <ari.rantalainen@nordcloud.com>
2024-04-16 15:24:45 +0300

    Added a timestamp to the reply

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ const app = express();
 //Set the port that you want the server to run on
 const PORT = process.env.PORT || 5000;
 
+const now = new Date();
+
 //creates an endpoint for the route /api
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from ExpressJS" });
+  res.json({ message: "Hello from ExpressJS - " + new Date().toISOString() });
 });
 
 // console.log that your server is up and running
